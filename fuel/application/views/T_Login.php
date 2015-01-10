5eccbcb6b2f788839d91063fc196ac1b869d80a7
Dave Gill <dave_gill@blueyonder.co.uk>
2015-01-10 20:38:56 +0000

    Tidied up login page

--- fuel/application/views/T_Login.php
+++ fuel/application/views/T_Login.php
@@ -1,3 +1,14 @@
 <script id="login-form" type="text/template">
+<div class="clearfix" id="page">
+   <a class="" id="u199" href="#">Home</a>
+    <a class="" id="u207" href="#artist-home">Artist Home</a>
+   <div class="verticalspacer"></div>
+   <div class="browser_width grpelem" id="u148-5-bw">
+    <div class="clearfix" id="u148-5">
+     <p>&nbsp;</p>
+     <p><span id="u148-2">Beatcrumb Inc 2014</span></p>
+    </div>
+   </div>
+  </div>
 
 </script>
