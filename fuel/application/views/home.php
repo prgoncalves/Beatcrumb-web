e102a4d76e4643c6c60f92706b536313be7c11bd
Dave Gill <dave@debian>
2015-01-10 12:58:32 +0000

    Fixed parse error

--- fuel/application/views/home.php
+++ fuel/application/views/home.php
@@ -1,2 +1,18 @@
 <?php 
 $this->load->view('templates');
+?>
+  <div class="clearfix" id="page"><!-- column -->
+   <div class="position_content" id="page_position_content">
+    <a class="nonblock nontext MuseLinkActive museBGSize colelem" id="u108" href="index.html"><!-- simple frame --></a>
+    <a class="nonblock nontext clearfix colelem" id="u118-4" href="login.html"><!-- content --><p>LOGIN</p></a>
+    <a class="nonblock nontext clearfix colelem" id="u119-4" href="artist-signup.html"><!-- content --><p>ARTIST SIGNUP</p></a>
+    <a class="nonblock nontext clearfix colelem" id="u120-4" href="fan-signup.html"><!-- content --><p>FAN SIGNUP</p></a>
+    <div class="verticalspacer"></div>
+    <div class="browser_width colelem" id="u148-5-bw">
+     <div class="rgba-background clearfix" id="u148-5"><!-- content -->
+      <p>&nbsp;</p>
+      <p><span id="u148-2">Beatcrumb Inc 2014</span></p>
+     </div>
+    </div>
+   </div>
+  </div>
