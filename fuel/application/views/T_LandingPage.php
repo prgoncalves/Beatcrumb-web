0a4f5f51b4fd5f6a9804fd16f8556abce1dcc74a
Dave Gill <dave_gill@blueyonder.co.uk>
2015-01-11 16:46:46 +0000

    Changed landing page to buttons

--- fuel/application/views/T_LandingPage.php
+++ fuel/application/views/T_LandingPage.php
@@ -1,17 +1,13 @@
 <script id="landing-page" type="text/template">
-  <div class="clearfix" id="page">
    <div class="position_content" id="page_position_content">
-    <a class="nonblock nontext MuseLinkActive museBGSize colelem" id="u108" href="index.html"></a>
-    <a class="nonblock nontext clearfix colelem" id="u118-4" href="#login"><p>LOGIN</p></a>
-    <a class="nonblock nontext clearfix colelem" id="u119-4" href="#artistsetup"><p>ARTIST SIGNUP</p></a>
-    <a class="nonblock nontext clearfix colelem" id="u120-4" href="#fansetup"><p>FAN SIGNUP</p></a>
-    <div class="verticalspacer"></div>
+    <a  href="#login"><button>LOGIN</button></a>
+    <a href="#artistsetup"><button>ARTIST SIGNUP</button></a>
+    <a href="#fansetup"><button>FAN SIGNUP</button></a>
     <div class="browser_width colelem" id="u148-5-bw">
-     <div class="rgba-background clearfix" id="u148-5"><!-- content -->
+     <div class="rgba-background clearfix" id="u148-5">
       <p>&nbsp;</p>
       <p><span id="u148-2">Beatcrumb Inc 2014</span></p>
      </div>
     </div>
    </div>
-  </div>
-</script>
+ </script>
