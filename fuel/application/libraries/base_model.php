953872c6725f7d9181efaf9e52f619740e230672
Dave Gill <dave_gill@blueyonder.co.uk>
2015-01-13 23:11:43 +0000

    Base model

---
+++ fuel/application/libraries/base_model.php
@@ -0,0 +1,63 @@
+<?php
+class base_model extends CI_Model{
+	protected $tableName; 
+	protected $keyField;
+	function __construct(){
+		parent::__construct();
+	}
+	public function delete($id){
+		$this->db->where($this->keyField,$id)->delete($this->tableName);
+		return $this->db->affected_rows();
+	}
+	public function update($data){
+		$keyfield = $this->keyField;
+		if (is_array($data)) {
+			$keyValue = $data[$keyfield];
+		} else {
+
+			$keyValue = $data->$keyfield;			
+		}
+		$this->db->where($this->keyField,$keyValue);
+		$this->db->update($this->tableName,$data);
+		if (is_array($data)) {
+			return $this->findOneById($data[$keyfield]);
+		} else {
+			return $this->findOneById($data->$keyfield);
+		}
+	}
+	public function create($data){
+		$this->db->insert($this->tableName,$data);
+		$id =  $this->db->insert_id();
+		return $this->findOneById($id);
+	}
+	public function findOneById($id){
+		$result = $this->db->where($this->keyField,$id)->get($this->tableName);
+		$data = $result->result();
+		if (isset($data) && !empty($data)){
+			return $data[0];
+		} else {
+			return null;
+		}
+	}
+	public function findAll(){
+		$result = $this->db->get($this->tableName);
+		return $result->result();
+	}
+	public function find($query){
+		$this->db->where($query);
+		$result = $this->db->get($this->tableName);
+		return $result->result();
+	}
+	public function findOneByQuery($query){
+		$result = $this->find($query);
+		return $result[0];
+	}
+}
+
+class project_model extends base_model{
+	public function findAllForProject($projectId){
+		$this->db->where('project_id',$projectId);
+		$result = $this->db->get($this->tableName);
+		return $result->result();			
+	}
+}
