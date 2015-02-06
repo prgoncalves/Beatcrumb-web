92d0807fee2134c2385508c138c0a7a2a1bedd5c
Dave Gill <dave_gill@blueyonder.co.uk>
2015-02-06 19:39:38 +0000

    Fan setup looks

--- fuel/application/views/T_FanSetup.php
+++ fuel/application/views/T_FanSetup.php
@@ -1,7 +1,10 @@
 <script id="fan-setup-form" type="text/template">
- <h1>Sign up for an Artist account</h1>
-   <p>Already have an account then <a href='#login'>Login</a></p>
-    <form class="artist-signup">
+  <div class="landing-page-wrapper">
+    <div class="landing-logo"></div>
+        <div class="form-container">
+ <h1 class="form-header">Sign up for an Fan account</h1>
+   <p class="form-login">Already have an account then <a href='#login'>Login</a></p>
+    <form class="artist-signup-form">
     	<label>What's your Email address</label>
     	<input type="text" name="email" placeholder="Email"></input>
     	<label>Choose a username</label>
@@ -10,7 +13,14 @@
     	<input type="text" name="password" placeholder="Password"></input>
     	<label>Artist/Band name</label>
     	<input type="text" name="artist" placeholder="Artist/and name"></input>
+	<div class="terms">
     	<p><Input type='checkbox' name='terms'></input>Yes send me Email updates about Beatcrumb.</p>
+	</div>
+	<div class="terms">
    	<p><Input type='checkbox' name='privacy'></input>I agree to the terms of use and privacy policy.</p>
+	</div>
     </form>
+	</div>
+	</div>
+	</div>
 </script>
