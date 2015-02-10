a2c1873c7aa2cbddb67c06eb770c9484ee558bf9
JamesRawding <jhrawding.w@gmail.com>
2015-02-10 22:07:23 +0000

    fan signup styling, needs extra tweaks for mobile

--- fuel/application/views/T_FanSetup.php
+++ fuel/application/views/T_FanSetup.php
@@ -4,7 +4,7 @@
         <div class="form-container">
  <h1 class="form-header">Sign up for an Fan account</h1>
    <p class="form-login">Already have an account then <a href='#login'>Login</a></p>
-    <form class="artist-signup-form">
+    <form class="fan-signup-form">
     	<label>What's your Email address</label>
     	<input type="text" name="email" placeholder="Email"></input>
     	<label>Choose a username</label>
@@ -13,11 +13,13 @@
     	<input type="text" name="password" placeholder="Password"></input>
     	<label>Artist/Band name</label>
     	<input type="text" name="artist" placeholder="Artist/and name"></input>
-	<div class="terms">
-    	<p><Input type='checkbox' name='terms'></input>Yes send me Email updates about Beatcrumb.</p>
+	<div class="fan-terms">
+    	<Input type='checkbox' name='terms'></input>
+        <p>Yes send me Email updates about Beatcrumb.</p>
 	</div>
-	<div class="terms">
-   	<p><Input type='checkbox' name='privacy'></input>I agree to the terms of use and privacy policy.</p>
+	<div class="fan-terms">
+        <Input type='checkbox' name='privacy'></input>
+   	<p>I agree to the terms of use and privacy policy.</p>
 	</div>
     </form>
 	</div>
