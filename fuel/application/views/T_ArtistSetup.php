6e85a62fcd509bf07c4a5967ab158ed02cdf796b
JamesRawding <jhrawding.w@gmail.com>
2015-02-12 21:01:24 +0000

    fan signup and login styling
    styled the fan setup form and cut the submit btn and feedback div from
    artist setup.
    
    setup the layout of login form, needs correct hrefs etc

--- fuel/application/views/T_ArtistSetup.php
+++ fuel/application/views/T_ArtistSetup.php
@@ -3,34 +3,34 @@
     <div class="landing-logo"></div>
         <div class="form-container">
           <h1 class="form-header">Sign up for an Artist account</h1>
-   <p class="form-login">Already have an account then <a href='#login'>Login</a></p>
-    <form class="artist-signup-form">
-    	<label>Username</label>
-    	<input type="text" name="username" id="username" placeholder="Username"></input>
-    	<label>Password</label>
-    	<input type="password" name="password" id="password" placeholder="Password"></input>
-    	<label>Email</label>
-    	<input type="text" name="email" id="email" placeholder="Email"></input>
-    	<label>Artist/Band name</label>
-    	<input type="text" name="artist" id="artist" placeholder="Artist/and name"></input>
-    	<div class="terms">
-    	<input type='checkbox' id="terms" name='terms'></input>
-        <p>I have read and agree to the terms of use.</p>
-    	</div>
-        <input id="submit-btn" type="submit" name="SignUp" value="Sign up"></input>
+          <p class="form-login">Already have an account then <a href='#login'>Login</a></p>
+            <form class="artist-signup-form">
+                <label>Username</label>
+                <input type="text" name="username" id="username" placeholder="Username"></input>
+                <label>Password</label>
+                <input type="password" name="password" id="password" placeholder="Password"></input>
+                <label>Email</label>
+                <input type="text" name="email" id="email" placeholder="Email"></input>
+                <label>Artist/Band name</label>
+                <input type="text" name="artist" id="artist" placeholder="Artist/and name"></input>
+                    <div class="terms">
+                        <input type='checkbox' id="terms" name='terms'></input>
+                        <p>I have read and agree to the terms of use.</p>
+                    </div>
+                <input id="submit-btn" type="submit" name="SignUp" value="Sign up"></input>
 		<div class="feedback"></div>
-    </form>
+           </form>
     
-    </div>
-
-    <div class="browser_width colelem" id="u148-5-bw">
-     <div class="clearfix" id="u148-5"><!-- content -->
-      <p>&nbsp;</p>
-      <div class="copyright-container">
-            <p><span id="u148-2">Beatcrumb Inc 2014</span></p>
         </div>
+
+        <div class="browser_width colelem" id="u148-5-bw">
+            <div class="clearfix" id="u148-5"><!-- content -->
+                <p>&nbsp;</p>
+                <div class="copyright-container">
+                    <p><span id="u148-2">Beatcrumb Inc 2014</span></p>
+                </div>
      
-    </div>
-    </div>
-    </div>
+            </div>
+        </div>
+  </div>
 </script>
