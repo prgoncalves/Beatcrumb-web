077968f76ff59d82a0ce689c7634f15fc4debf60
Dave Gill <dave_gill@blueyonder.co.uk>
2015-03-30 22:34:26 +0100

    Take invalid @ from cookie

--- fuel/application/config/config.php
+++ fuel/application/config/config.php
@@ -244,13 +244,13 @@ $config['encryption_key'] = 'xknd@e_9iiuds!diuukdndkjasbdue8';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'B3@tcrumb';
+$config['sess_cookie_name']		= 'CvegyuxbnUbneeubUbewebewn';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
-$config['sess_match_ip']		= FALSE;
+$config['sess_match_ip']		= TRUE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
 
