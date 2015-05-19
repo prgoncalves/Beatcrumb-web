fadce4af851d6f42fee5b51da1d1ade83b32cbb2
Dave Gill <dave_gill@blueyonder.co.uk>
2015-05-19 11:54:21 +0100

    Fixed typo on activation error page

--- fuel/application/views/activation_error.php
+++ fuel/application/views/activation_error.php
@@ -2,7 +2,7 @@
 	<div class="landing-logo"></div>
 	<div class="error activation">
 		<h3>Activation Error</h3>
-		<p>Your account has could not be activated at the moment.  Please try again or contact the Beatcrumb team to see what went wrong.</p>
+		<p>Your account could not be activated at the moment.  Please try again or contact the Beatcrumb team to see what went wrong.</p>
 		<p>Don't forget to share the love.</p>
 	</div>
 </div>
