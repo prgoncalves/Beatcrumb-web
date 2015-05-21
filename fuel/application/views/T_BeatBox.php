0ee6a5fe5a78cce77106de034f67e627dd6d6cfb
Dave Gill <dave_gill@blueyonder.co.uk>
2015-05-21 10:15:56 +0100

    Styling the beatbox - A bit

--- fuel/application/views/T_BeatBox.php
+++ fuel/application/views/T_BeatBox.php
@@ -3,16 +3,26 @@
     <div class="main-container">
 		<div class="beatbox">
 			<%_.each(app.availableInbox, function(value) { console.log(value);%>
-				<%=value.artist_name%>
-				<%=value.filename%>
-				<%=value.plays%>
-				<%=value.shares%>
-				<%=value.message%>
-				<%if (value.image){%>
-					<div class="artist-mini-picture">
-						<img src="assets/<%=value.image%>" />
+				<div class="beatbox-track">
+					<h2 class="beatbox-artist-name"><%=value.artist_name%></h2>
+					<div class="beatbox-track-name"><%=value.filename%></div>
+					<div class="beatbox-listens"><%=value.plays%></div>
+					<div class="beatbox-shares"><%=value.shares%></div>
+					<div class="beatbox-message"><%=value.message%></div>
+					<%if (value.image){%>
+						<div class="artist-mini-picture">
+							<img src="assets/<%=value.image%>" />
+						</div>
+					<%}%> 
+					<div class="beatbox-single-crumb">
+				   		<div class="beatbox-crumb active-crumb"></div>
 					</div>
-				<%}%> 
+					<div class="beatbox-three-crumb">
+                		<div class="three-crumb-one"></div>
+                    	<div class="three-crumb-two"></div>
+                    	<div class="three-crumb-three"></div>
+                	</div>
+				</div>
 			<% }); %>											
 		</div>
 	</div>
