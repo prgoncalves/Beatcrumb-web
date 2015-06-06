88794c3629224e27e746a1e2d305fe5925f85d4d
Dave Gill <dave_gill@blueyonder.co.uk>
2015-06-06 11:17:44 +0100

    Added bit for added contact

--- fuel/application/views/T_AddContact.php
+++ fuel/application/views/T_AddContact.php
@@ -1,13 +1,13 @@
 <script id="addContact" type="text/template">
 	<div class="settings-profile">
 		<form class='add-contact'>
-			<% if (email) { %>
+			<% if (typeof email !== 'undefined') { %>
 				<h2>Edit Contact</h2>
 			<% } else { %>
 				<h2>Add Contact</h2>
 			<% } %>
 			<label>Email Address</labeL>
-			<input type='text' name='email' value="<%=email%>"/>
+			<input type='text' name='email' value="<%if (typeof email !== 'undefined'){%><%=email%><%}%>"/>
 			<label>Contact Name</label>
 			<input type='text' name='name' value=""/>
 			<button class='js-saveContact'>Save</button>
