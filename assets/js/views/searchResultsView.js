fa17849b95f79bf3941f659ec0bb19bc5559cefe
Dave Gill <dave_gill@blueyonder.co.uk>
2015-07-13 22:06:05 +0100

    Skeleton code for sharing from search results

--- assets/js/views/searchResultsView.js
+++ assets/js/views/searchResultsView.js
@@ -12,7 +12,10 @@ SearchResults = Backbone.View.extend({
 		this.$el.html(content);
 	},
 	events : {
-		'click .playTrack' : 'playMP3'
+		'click .playTrack'     : 'playMP3',
+		'click .shareTrack'    : 'showShareDialog',
+		'click .js-doShare'    : 'shareTrack',
+		'click .js-ReleaseToMe': 'shareWith',
 	},
 	playMP3 : function (e){
 		e.preventDefault();
@@ -30,6 +33,76 @@ SearchResults = Backbone.View.extend({
 		});
 		mySoundObject.play();
 	return false;
-},
+	},
+	showShareDialog : function(ev){
+		var target = $(ev.target);
+		this.trackId = $(target).data('id');
+		// remove active crumb from all
+		$('.discover-crumb').removeClass('active_crumb');
+		$(target).addClass('active-crumb');
+		// show contacts........
+		$('.release-form').show();
+		$('.fans-scroll').show();		
+	},
+	shareTrack : function(ev){
+		var contacts = [];
+		$('.ShareWithMe').each(function(){
+			contacts.push($(this).data('id'));
+		});	
+		if ($('#shareMessage').val().length > 0){
+			if (contacts.length > 0){
+				var data = {
+						track    : this.trackId,
+						contacts : contacts,
+						message  : $('#shareMessage').val()
+					}
+				$.ajax({
+				    url: 'api/r/tracks/share',
+				    data: data,
+				    dataType : 'json',
+				    success: function(Result){
+				    	switch(Result.Status){
+				    		case 'OK'  : 
+				    			app.message('Your tracks have been shared.');
+				    			$('.release-form').hide();
+				    			$('.fans-scroll').hide();
+				    			$("div").removeClass('ShareMe');
+				    			$("div").removeClass('active-crumb');			    			
+				    			break;
+				    		case 'ERR' : app.alert('Share Failed');break;
+				    		case 'LOG' : app.appRouter.navigate('/login',true);
+				    	}
+				    	
+				    	if (Result.Status == 'ERR'){
+					    	app.alert('Shared failed');			    		
+				    	}
+				    },
+				    error: function(data){
+				    	app.alert('Shared failed');
+				    }
+			 });
+			} else {
+				app.alert('Please select some contacts to share this track with!');
+			}			
+		} else {
+			app.alert('Please add a message for sharing this track!');			
+		}
+		
+	},
+	shareWith : function(e){
+		e.preventDefault();
+		var container = $(e.target).closest('.fan-info');
+		var crumb = $(container).children('.fan-crumb');
+		if ($(crumb).hasClass('active-crumb')){
+			$(crumb.removeClass('active-crumb'));
+		} else {
+			$(crumb.addClass('active-crumb'));			
+		}
+		if ($(container).hasClass('ShareWithMe')){
+			$(container).removeClass('ShareWithMe');						
+		} else {
+			$(container).addClass('ShareWithMe');			
+		}
+	},
 	
 });
