110e6f798eabdbbecbe9d125719d4648405310e8
Dave Gill <dave_gill@blueyonder.co.uk>
2015-07-20 21:47:15 +0100

    Basic unit testing using codeigniter library

--- fuel/application/controllers/test.php
+++ fuel/application/controllers/test.php
@@ -3,32 +3,39 @@ define('TESTING', true);
 class Test extends CI_Controller{
 	function __construct(){
 		parent::__construct();
-		$this->load->libary('unit_test');
+		$this->load->library('unit_test');
+	}
+	public function testLogin(){
+		$this->load->model('user_model','user');
+		$result = $this->user->login('s',md5('s'));
+		$this->unit->run($result,'is_array','Testing login...');
+	}
+	public function index(){
+		$this->load->model('user_model','user');
+		$this->test();
+		// Order of these is important.
+		$this->testLogin();
+		$this->getContacts();
+		$this->testForgottenPassword();
+		$this->getTracks();
+		echo $this->unit->report();
 	}
 	public function test(){
-		echo('Testing');
+		$this->unit->run(true,true,'Just testing the test system!');
 	}
 	public function testForgottenPassword(){
-		echo("Calling testforgottenpassword");
 		$this->load->model('user_model','user');
-		echo("Model loaded and about to call function ");
-		$result = $this->user->forgottenPassword('freds@freds.com','artist');
-		$result = $this->user->forgottenPassword('dave_gill@blueyonder.co.uk','fan');
-		echo($result);
+		$result = $this->user->forgottenPassword('x@x.com','artist');
+		$this->unit->run(false,$result,'Testing forgotten password on an account that does not exist');
 	}
 	public function getTracks(){
-		echo("Loading model");
 		$this->load->model('artist_model','artist');
-		echo('Calling function to get tracks<br>');
 		$result = $this->artist->tracks('41b20af3b53ded6ffed3e7ac39aad0df0af0835d0431b136e980113446c273099117ae5eca768107d1a6783df331c4403a4da08524eb72b6447b6e29181f9f21045f3696852941aa538a74fbe93272d99479f8a9de86bb2f6ece01c850d0e223abbdbfcf');
-		foreach($result as $track){
-			echo($track->filename);
-			echo("<br>");
-		}
+		$this->unit->run($result,'is_array','Testing the fetching of tracks!',print_r($result,true));
 	}
 	public function getContacts(){
 		$this->load->model('contacts_model','contacts');
 		$result = $this->contacts->getContactsForUUID();
-		var_dump($result);
+		$this->unit->run($result,'is_array','Testing the fetching of contacts!',print_r($result,true));
 	}
 }
