97d131e63d6a718b1e84afa37c0c5c127e35cdd1
Dave Gill <dave_gill@blueyonder.co.uk>
2015-09-24 20:15:06 +0100

    Filter discover tracks to add Playable field

--- fuel/application/models/genre_model.php
+++ fuel/application/models/genre_model.php
@@ -7,10 +7,12 @@ class Genre_model extends Base_module_model {
 	{
 		parent::__construct('genre');
 	}
-	function getTracksForGenre($id){
-		$this->db->select('artist_name,uuid,tracks.id,plays,shares,filename,artist.image');
+	function getTracksForGenre($id,$uuid){
+		// need to change this so that it gets a flag to see if the track can be played or shared.
+		$this->db->select('artist_name,uuid,tracks.id,plays,shares,filename,artist.image,playable');
 		$this->db->where('genre',$id);
 		$this->db->join('artist','tracks.artist_id = artist.id');
+		$this->db->join('user_played','user_played.track_id = tracks.id and user_played.uuid = "' . $uuid . '"','LEFT');
 		$data = $this->db->get('tracks')->result();
 		return $data;
 	}
